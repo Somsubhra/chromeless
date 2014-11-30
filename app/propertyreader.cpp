5eef91d6627310fdaf071453248fa6a6ee864927
Somsubhra Bairi <somsubhra.bairi@gmail.com>
2014-11-30 19:39:39 +0530

    Add getters for App Properties.

--- app/propertyreader.cpp
+++ app/propertyreader.cpp
@@ -1,3 +1,10 @@
+// Qt includes
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QDebug>
+
+// Other includes
 #include "propertyreader.h"
 
 PropertyReader::PropertyReader(QObject *parent) :
@@ -7,5 +14,90 @@ PropertyReader::PropertyReader(QObject *parent) :
 
 void PropertyReader::readProperties(QString propFile)
 {
+    QFile file(propFile);
+    file.open(QIODevice::ReadOnly);
 
+    QJsonParseError err;
+    QJsonDocument doc = QJsonDocument::fromJson(file.readAll(), &err);
+
+    // If error in parsing the document, then return
+    if(err.error != QJsonParseError::NoError) {
+        return;
+    }
+
+    QJsonObject jObj = doc.object();
+
+    qDebug() << jObj["author"].toObject()["name"].toString();
+
+    file.close();
+}
+
+QString PropertyReader::appVersion()
+{
+    return _appVersion;
+}
+
+QString PropertyReader::appName()
+{
+    return _appName;
+}
+
+QString PropertyReader::appDesc()
+{
+    return _appDesc;
+}
+
+QString PropertyReader::authorName()
+{
+    return _authorName;
+}
+
+QString PropertyReader::authorEmail()
+{
+    return _authorEmail;
+}
+
+int PropertyReader::windowHeight()
+{
+    return _winHeight;
+}
+
+int PropertyReader::windowWidth()
+{
+    return _winWidth;
+}
+
+int PropertyReader::windowMaxWidth()
+{
+    return _winMaxWidth;
+}
+
+int PropertyReader::windowMaxHeight()
+{
+    return _winMaxHeight;
+}
+
+int PropertyReader::windowMinWidth()
+{
+    return _winMinWidth;
+}
+
+int PropertyReader::windowMinHeight()
+{
+    return _winMinHeight;
+}
+
+int PropertyReader::windowFixedWidth()
+{
+    return _winFixedWidth;
+}
+
+int PropertyReader::windowFixedHeight()
+{
+    return _winFixedHeight;
+}
+
+bool PropertyReader::windowShowTitleBar()
+{
+    return _winShowTitleBar;
 }
--- app/propertyreader.h
+++ app/propertyreader.h
@@ -1,6 +1,7 @@
 #ifndef PROPERTYREADER_H
 #define PROPERTYREADER_H
 
+// Qt includes
 #include <QObject>
 
 class PropertyReader : public QObject
@@ -10,6 +11,41 @@ public:
     explicit PropertyReader(QObject *parent = 0);
 
     void readProperties(QString propFile);
+
+    QString appVersion();
+    QString appName();
+    QString appDesc();
+
+    QString authorName();
+    QString authorEmail();
+
+    int windowHeight();
+    int windowWidth();
+    int windowMaxWidth();
+    int windowMaxHeight();
+    int windowMinWidth();
+    int windowMinHeight();
+    int windowFixedWidth();
+    int windowFixedHeight();
+    bool windowShowTitleBar();
+
+private:
+    QString _appVersion;
+    QString _appName;
+    QString _appDesc;
+
+    QString _authorName;
+    QString _authorEmail;
+
+    int _winHeight;
+    int _winWidth;
+    int _winMaxWidth;
+    int _winMaxHeight;
+    int _winMinWidth;
+    int _winMinHeight;
+    int _winFixedWidth;
+    int _winFixedHeight;
+    bool _winShowTitleBar;
 
 signals:
 
